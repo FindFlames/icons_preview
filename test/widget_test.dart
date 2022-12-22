3385ebabdcb2001d5667a2a05a976ed65f3c90b6
Saumya Kumar <53995365+saumyakr1232@users.noreply.github.com>
2022-12-22 18:44:47 +0530

    added empty test

---
+++ test/widget_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('MyWidget has a title and message', (tester) async {
+
+    expect(1, 1);
+  });
+
+}
